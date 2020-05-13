6dc1232a5b2e043068f4d69160d3bdbd7dad580e
bsinoimeri <b.sinoimeri@hotmail.com>
2020-05-13 17:31:12 +0200

    Fifth part of Django official tutorial

--- src/articles/views.py
+++ src/articles/views.py
@@ -10,27 +10,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.filter(pub_date__lte=timezone.now() ).order_by('-pub_date')[:5]
+        return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'articles/detail.html'
 
- def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'articles/results.html'
 
-     def get_queryset(self):
-        """
-        Excludes any questions that aren't published yet.
-        """
-        return Question.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
